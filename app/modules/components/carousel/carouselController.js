6ea0d10cd424a42148aac2889519ed3bcab8f2ab
ericsvendsen <esvendsen@gmail.com>
2017-02-19 23:50:32 -0500

    removing bootstrap

--- app/modules/components/carousel/carouselController.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('ngwordpress').controller('carouselController', function ($scope, wpMedia, _) {
-        $scope.sliderImages = _.filter(wpMedia, { caption: 'carousel' });
-    });
-})();
